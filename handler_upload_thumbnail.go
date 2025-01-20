0a78a364d8f9cda38f920f994c153b15d6e6d19a
Rareshp <155653633+Rareshp@users.noreply.github.com>
2025-01-20 19:54:19 +0200

    handle thumbnail uploads

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+  "time"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	_ "github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
+
+// 10 MB
+const maxMemory int = 10 << 20;
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
@@ -28,10 +34,60 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+  err = r.ParseMultipartForm(int64(maxMemory))
+  if err != nil {
+    respondWithError(w, http.StatusInternalServerError, "Something went wrong parsing form", err)
+    return
+  }
+  // the above is needed first
+  imageData, header, err := r.FormFile("thumbnail");
+  if err != nil {
+    respondWithError(w, http.StatusInternalServerError, "Something went wrong parsing form", err)
+    return
+  }
+  defer imageData.Close()
 
-	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+  // generate thumb 
+  imageDataInBytes, err := io.ReadAll(imageData);
+  if err != nil {
+    respondWithError(w, http.StatusInternalServerError, "Something went wrong parsing image data", err)
+    return
+  }
+  // example "image/png"
+  mediaType := header.Header.Get("Content-Type");
+  if mediaType == "" {
+		respondWithError(w, http.StatusBadRequest, "Missing Content-Type for thumbnail", nil)
+    return
+  }
+  thumb := thumbnail{
+    data: imageDataInBytes, 
+    mediaType: mediaType,
+  }
 
-	// TODO: implement the upload here
+  videoMetadata, err := cfg.db.GetVideo(videoID);
+  if err != nil {
+    respondWithError(w, http.StatusUnauthorized, "Couldn't find video", err);
+  }
+	if videoMetadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Not authorized to update this video", nil)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+  // add to videomap in memory 
+  videoThumbnails[videoID] = thumb;
+
+  // update video in database 
+  thumbnailURL := fmt.Sprintf("http://localhost:%d/api/thumbnails/%s", cfg.port, videoID)
+
+  videoMetadata.ThumbnailURL = &thumbnailURL;
+  videoMetadata.UpdatedAt = time.Now();
+
+  err = cfg.db.UpdateVideo(videoMetadata);
+  if err != nil {
+    delete(videoThumbnails, videoID)
+    respondWithError(w, http.StatusInternalServerError, "Failed to update video", err)
+    return
+  }
+
+	respondWithJSON(w, http.StatusOK, videoMetadata)
 }
